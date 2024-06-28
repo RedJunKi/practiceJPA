e2dd19c8edd3dd991a8b0f0a18c15c46e5a94935
RedJunKi <bing8821@naver.com>
2024-06-28 14:45:40 +0900

    [FEAT] 영속성 전이 적용, fetch 전략 설정

--- src/main/java/com/jpa/practiceJPA/order/Order.java
+++ src/main/java/com/jpa/practiceJPA/order/Order.java
@@ -23,11 +23,11 @@ public class Order extends BaseEntity {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 
     @Temporal(TemporalType.TIMESTAMP)
@@ -36,7 +36,7 @@ public class Order extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
